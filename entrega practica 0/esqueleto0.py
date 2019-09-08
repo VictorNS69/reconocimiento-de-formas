5ccd4bcb70d5b106ebbd5334c7c9f3e3500947a9
Victor Nieves Sanchez <vnievess@gmail.com>
2019-09-08 19:39:51 +0200

    Created matrices reading the file

--- entrega practica 0/esqueleto0.py
+++ entrega practica 0/esqueleto0.py
@@ -1,5 +1,5 @@
-
 import numpy as np
+
 
 class Classifier:
     def __init__(self):
@@ -11,32 +11,37 @@ class Classifier:
     def predict(self):
         pass
 
+
 class classifEuclid(Classifier):
-    def __init__(self,labels=[]):
+    def __init__(self, labels=[]):
         """Constructor de la clase
         labels: lista de etiquetas de esta clase"""
-        self.labels=labels
+        self.labels = labels
         pass
 
-    def fit(self,X,y):
+    def fit(self, x, y):
         """Entrena el clasificador
         X: matriz numpy cada fila es un dato, cada columna una medida
         y: vector de etiquetas, tantos elementos como filas en X
         retorna objeto clasificador"""
         return self
 
-    def predict(self,X):
+    def predict(self, x):
         """Estima el grado de pertenencia de cada dato a las clases 
         X: matriz numpy cada fila es un dato, cada columna una medida
         retorna una matriz, cada fila almacena los valores pertenencia""" 
         return "..."
     
-    def predLabel(self,X):
+    def predLabel(self, x):
         """Estima la etiqueta de cada dato
         X: matriz numpy cada fila es un dato, cada columna una medida
         retorna un vector con las etiquetas de cada dato"""
         return "..."
 
-# Para leer los datos
-samples = np.genfromtxt("irisData.txt", "...")
-labels = np.genfromtxt("irisData.txt", "...")
+
+# Read the data
+samples = np.genfromtxt("irisData.txt", usecols=(0, 1, 2, 3), dtype=float)  # 150x4 Matrix
+labels = np.genfromtxt("irisData.txt", usecols=[-1], dtype=str)             # 150x1 Matrix (Vector)
+
+print("Sample matrix dimension", samples.shape)
+print("Labels matrix dimension", labels.shape)
